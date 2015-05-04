97c9130f927bdffec922dd54a099b2c5255683c5
dudhaneviraj <dudhaneviraj@gmail.com>
2015-05-04 02:17:46 -0700

    "Commiting Charts.java to Code"

--- src/main/java/com/spring/controller/Charts.java
+++ src/main/java/com/spring/controller/Charts.java
@@ -9,22 +9,43 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+import edu.sjsu.cmpe283.util.MongoDBConnection;
 @Controller
 public class Charts {
-	
+
 	@RequestMapping(value = "/performance", method = RequestMethod.GET)
 	public ModelAndView getPerformance()throws Exception
 	{
-		ModelAndView m=new ModelAndView("performance");
-		//DBCollection col=getCollection("");
 		ArrayList<String>  vm=new ArrayList<String>();
+		ArrayList<String> vCPU=new ArrayList<String>();
+		ArrayList<String> iP=new ArrayList<String>();
+		ModelAndView m=new ModelAndView("performance");
+		//Mongo Fetch Code
+		DBCollection col=MongoDBConnection.db.getCollection("performance");
+		DBCursor cursor=col.find();
+		System.out.println(col.count());
+		while(cursor.hasNext())
+		{
+			DBObject ob=cursor.next();
+			vm.add(ob.get("VM Name").toString());
+			vCPU.add(ob.get("vCPU usage").toString());
+			iP.add(ob.get("VM IP").toString());
+		}
+
 		vm.add("VM1");
-		vm.add("VM2");
-		ArrayList<String> vCPU=new ArrayList<String>();	
+		vm.add("VM2");	
 		vCPU.add("88");
 		vCPU.add("74");
+		iP.add("192.168.10.12");
+		iP.add("192.168.10.12");
+		iP.add("192.168.10.12");
+		// Adding Attrbutes
 		m.addObject("VM", vm);
 		m.addObject("cpu", vCPU);
+
 		return m;
 	}
 
